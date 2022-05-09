5b1d06d0f5ba08621f64c3cd08387746710543dc
frederico pires <free.pires@hotmail.com>
2022-05-09 15:53:44 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.media.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 4;
+        System.out.println("alomundo" + (a+b));*/
+
+    }
+}
